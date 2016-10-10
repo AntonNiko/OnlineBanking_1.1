60e97ee58ce65e4f84998c70e8ab29e9db8baef9
Anton Nikitenko <ant.nikitenko@gmail.com>
2016-10-10 11:20:47 -0700

    Add files via upload

---
+++ resources/php/logout.php
@@ -0,0 +1,15 @@
+<?php 
+session_start();
+
+if(isset($_POST["logout"])){
+	if(isset($_SESSION["loggedIn"])){
+		unset($_SESSION["loggedIn"]);
+		unset($_SESSION["ipAddress"]);
+		header("Location: http://localhost");
+	}else{
+		header("Location: http://localhost");
+	}
+}
+
+
+?>
